12903e8809bc99805f94a163c37ac01ae8481103
chayaphon007 <earthshaker1666@gmail.com>
2025-07-18 11:23:41 +0700

    add 02agent

--- 02agent/agent.py
+++ 02agent/agent.py
@@ -1,13 +1,19 @@
 from google.adk.agents import Agent
 from google.adk.code_executors import BuiltInCodeExecutor
-
-code_agent = Agent(
-    name="code_agent",
-    model=MODEL,
-    description="A coding assistant that can write and debug Python code.",
-    instruction="You are a coding assistant. Write and execute python code to solve the user's problem.",
-    tools=[BuiltInCodeExecutor()],
+from google.genai import types
+ 
+root_agent = Agent(
+    name="ag02",
+    model="gemini-2.0-flash",
+    description="Agent that executes Python and Java codes.",
+    instruction=(
+        "You are a helpful agent who can:\n"
+        "You are a coding assistant. Write and execute:\n"
+        "- Execute Python code to generate answers (e.g., Fibonacci or other functions)\n"
+        "- Execute Java code for dynamic programming tasks (e.g., creating classes, methods, and running Java programs)\n"
+    ),
     generate_content_config=types.GenerateContentConfig(
         temperature=0.2,
     ),
+    code_executor=BuiltInCodeExecutor()
 )
